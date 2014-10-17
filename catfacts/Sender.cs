d3f2ce3eebd926bb7433cb26d231c58a0978b1d4
Dan Leonard <daniel@danleonard.us>
2014-10-17 18:03:15 -0500

    Added "Sender" class

---
+++ catfacts/Sender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace catfacts
+{
+    class Sender
+    {
+        public Sender(string googleAccountEmail, string googleAccountPassword, string testNum, string test2)
+        {
+
+        }
+    }
+}
