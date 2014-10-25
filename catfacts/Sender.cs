574b9548cea9abd310118eda3cffc7ef8e8d78a6
Dan Leonard <daniel@danleonard.us>
2014-10-25 10:53:24 -0500

    Secr

---
+++ catfacts/Sender.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace catfacts
+{
+    class Sender
+    {
+        public  string  Username    { get; set; }
+        public  string  Password    { get; set; }
+
+        private string  debugUsername   =   "crazyworkoutkid";
+
+        public void ConsoleLogin()
+        {
+            // Get the username
+            Console.Write("Username: ");
+#if DEBUG
+            // If in debug mode, use the debugUsername as the username
+            Username = debugUsername;
+            Console.WriteLine(Username);
+#else
+            // If in release mode, read password from console input
+            Username = Console.ReadLine();
+#endif
+
+            // Get the password
+            Console.Write("Password: ");
+            Password = Console.ReadLine();
+        }
+    }
+}
