11755adf9bdf03442a4623f8da796105bafcf022
Dan Leonard <daniel@danleonard.us>
2014-11-20 11:13:10 -0600

    Added service

---
+++ catfacts-service/Service1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace catfacts_service
+{
+    public partial class catfacts_service : ServiceBase
+    {
+        public catfacts_service()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
